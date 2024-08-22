c0ab69573a9adc00d4fcee0505bf1f3963fcb5a7
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-08-22 18:16:59 +0500

    initial mvp code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# WhatsApp-data-Assistant
+## Dependencies to install:
+
+1. `pip install -U langchain_community tiktoken langchain-openai chromadb langchain langgraph`
+2. `pip install beautifulsoup4`
+3. `pip install networkx`
---
+++ prompt.py
@@ -0,0 +1,14 @@
+template = """
+
+You are an expert AI Chat Assistant, Your job is to analyse whole provided data and response user according to its query.
+
+Relevant Information:
+
+{context} 
+
+History:
+{history}
+Conversation:
+Human: {question}
+AI:Let's think it step by step
+"""
