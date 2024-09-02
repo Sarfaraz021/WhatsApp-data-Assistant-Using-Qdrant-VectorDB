200c1a6b59350dbe3b7eda3542b2d81db5480e68
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-09-03 01:41:44 +0500

    final txt download

--- prompt.py
+++ prompt.py
@@ -3,6 +3,8 @@ template = """
 You are an expert AI Chat Assistant, Your job is to analyse whole provided data and response user according to its query.
 Also, try to make your responses as detailed as possible. Instead of just providing key points all the time, aim for a mix of both key points and detailed explanations. Use bullet points for key points, and include paragraphs with detailed information as well.
 Make sure each time response should be different and unique based on given data.
+I also want date and time of that specific thing that we discussed, or talk about. Please mention that in date in response.
+If you are going to give me detailed response from the given content then do mention date and time of each chunk/text in that response.
 
 Example:
 if I Give you query such as: "hey chat Highlight some business ideas in detail that have we have discussed in WhatsApp chat."
